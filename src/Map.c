9d113f34a43244602dbcdf35be0542f5e6f3356d
MertCengiz <118610270+MertCengiz@users.noreply.github.com>
2023-04-15 21:18:11 +0300

    Update Map.c

--- src/Map.c
+++ src/Map.c
@@ -1,42 +1,43 @@
 #include "map.h"
 
-int getInt(char * char_pointer, StrInt map){
-    for (int i = 0; i < map.count; i++){
-        if (strcmp(char_pointer, map.string[i]) == 0){
-            return map.IntMap[i];
+int getInt(char * char_pointer){
+    for (int i = 0; i < StrInt.count; i++){
+        if (strcmp(char_pointer, StrInt.string[i]) == 0){
+            return StrInt.IntMap[i];
         }
     }
     return -1;
 }
 
-void setInt(int index, int value, StrInt map){
-    map.IntMap[index] = value;
+void setInt(int index, int value){
+    StrInt.IntMap[index] = value;
 }
 
-int StrExists(char * char_pointer, StrInt map){
-    for (int i = 0; i < map.count; i++){
-        if (strcmp(char_pointer, map.string[i]) == 0){
+int StrExists(char * char_pointer){
+    for (int i = 0; i < StrInt.count; i++){
+        if (strcmp(char_pointer, StrInt.string[i]) == 0){
             return 0;
         }
     }
     return -1;
 }
 
-char *getString(ASTNode * node, AstStr map){
-    for (int i = 0; i < map.count ; i++){
-        if ((& map.ASTMap[i]) == (& node)){
-            return map.string[i];
+char *getString(ASTNode * node){
+    for (int i = 0; i < AstStr.count ; i++){
+        if (AstStr.ASTMap[i] == node){
+            return AstStr.string[i];
         }
     }
+    return -1;
 }
 
-void setString(int index, char * value, AstStr map){
-    map.string[index] = value;
+void setString(int index, char * value){
+    AstStr.string[index] = value;
 }
 
-int ASTExists(ASTNode * node, AstStr map){
-    for (int i = 0; i < map.count ; i++){
-        if ((& map.ASTMap[i]) == (& node)){
+int ASTExists(ASTNode * node){
+    for (int i = 0; i < AstStr.count ; i++){
+        if (AstStr.ASTMap[i] == node){
             return 0;
         }
     }
